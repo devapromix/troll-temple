2a7cdd7ce3a8e1c03694621fbc8ddc4554b03fe1
Gandifil <gandifilmk@gmail.com>
2023-11-02 15:45:27 +0300

    Add universal modifier

---
+++ src/common/modifiers/mod.py
@@ -0,0 +1,17 @@
+from .modifier import Modifier
+
+
+class Mod(Modifier):
+    def __init__(self, attr_name, value):
+        self.attr_name = attr_name
+        self.value = value
+
+    @property
+    def descr(self):
+        return '+%d %s' % (self.value, self.attr_name)
+
+    def commit(self, mob):
+        setattr(mob, self.attr_name, getattr(mob, self.attr_name) + self.value)
+
+    def rollback(self, mob):
+        setattr(mob, self.attr_name, getattr(mob, self.attr_name) - self.value)
