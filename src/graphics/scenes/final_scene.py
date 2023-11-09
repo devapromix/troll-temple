1c2a9e6b0ef133e6f204cf9eb90b191ffdac592f
devapromix <maxwof@ukr.net>
2023-11-09 10:54:29 +0200

    Refactor final scene.

--- src/graphics/scenes/final_scene.py
+++ src/graphics/scenes/final_scene.py
@@ -1,15 +1,15 @@
-import tcod as T
+from graphics.color import Color
 
 class FinalScene:
     def __init__(self, player):
         self.player = player
 
     def show(self):
-        from common.game import clear, out_file, refresh, out, anykey, Quit, COLOR_TITLE
+        from common.game import clear, out_file, refresh, out, anykey, Quit
         clear()
-        out(0, 2, "The end...", COLOR_TITLE)
-        out_file(10, 4, '../assets/texts/final.txt', T.lighter_grey)
-        out(0, 28, "Press [ENTER] to exit...", T.light_grey)
+        out(0, 2, "The end...", Color.TITLE.value)
+        out_file(10, 4, '../assets/texts/final.txt', Color.ITEM.value)
+        out(0, 28, "Press [ENTER] to exit...", Color.ITEM.value)
         refresh()
         anykey()
         raise Quit()
