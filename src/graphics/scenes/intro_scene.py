dd2fbe397ed61f4b7b60955dc8fa2f50f27ec662
devapromix <maxwof@ukr.net>
2023-11-10 23:45:59 +0200

    Refactor intro scene.

--- src/graphics/scenes/intro_scene.py
+++ src/graphics/scenes/intro_scene.py
@@ -1,12 +1,16 @@
+from .single_page_scene import SinglePageScene
 from graphics.color import Color
 
-class IntroScene:
-    def show(self):
-        from common.game import clear, out_file, refresh, out, anykey
-        clear()
+class IntroScene(SinglePageScene):
+    def _draw_content(self) -> None:
+        from common.game import out_file, out
         out(0, 2, "Many centuries ago...", Color.TITLE.value)
         out_file(10, 4, '../assets/texts/help.txt', Color.ITEM.value)
         out(0, 28, "Press [ENTER] to continue...", Color.ITEM.value)
-        refresh()
-        anykey()
+
+
+
+
+
+
 
