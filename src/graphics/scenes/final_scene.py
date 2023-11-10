1df9fb7d0f13fa8d89168695868e06c5fe455a42
devapromix <maxwof@ukr.net>
2023-11-10 23:53:45 +0200

    Refactor final scene.

--- src/graphics/scenes/final_scene.py
+++ src/graphics/scenes/final_scene.py
@@ -1,16 +1,19 @@
+import tcod as T
+from .single_page_scene import SinglePageScene
 from graphics.color import Color
 
-class FinalScene:
+class FinalScene(SinglePageScene):
     def __init__(self, player):
+        super().__init__()
         self.player = player
 
     def show(self):
-        from common.game import clear, out_file, refresh, out, anykey, Quit
-        clear()
+        from common.game import out_file, out
         out(0, 2, "The end...", Color.TITLE.value)
         out_file(10, 4, '../assets/texts/final.txt', Color.ITEM.value)
         out(0, 28, "Press [ENTER] to exit...", Color.ITEM.value)
-        refresh()
-        anykey()
-        raise Quit()
-        
+
+    def _check_input(self, key: int) -> bool:
+        from common.game import Quit
+        super()._check_input(key)
+        raise Quit()        
