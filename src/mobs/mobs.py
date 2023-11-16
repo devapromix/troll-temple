b6d60d8f54df306b10bfb539324c921e71ff495f
devapromix <maxwof@ukr.net>
2023-11-16 23:24:28 +0200

    Refactoring: final boss died.

--- src/mobs/mobs.py
+++ src/mobs/mobs.py
@@ -1,6 +1,5 @@
 import pygame
 from .monster import *
-from graphics.scenes.info_scene import InfoScene
 
 class FlyMonster(Monster):
     ABSTRACT = True
@@ -546,18 +545,13 @@ class TrollKing(FinalBossMonster):
 
     def __init__(self):
         super().__init__()
-        self.info_scene = InfoScene()
 
     def die(self, dmg):
         from items.amulets import RubyAmulet
-        from common.game import prompt, new_ui_turn
         super().die(dmg)
         self.tile.items.append(ScrollWhitePortal())
         self.tile.items.append(RubyAmulet())
         self.adv_drop()
         self.rare_drop()
         self.unique_drop()
-        if prompt('You have defeated the Troll King! Press [ENTER] to continue...', [pygame.K_RETURN]) == pygame.K_RETURN:
-            self.info_scene.message("You have defeated True Evil!", "You have come a long way and defeated the terrible tyrant Troll King! Now all the magical power of the Ruby Amulet is in your hands and the White Portal will show you the way home...")
-            self.info_scene.show()
-            new_ui_turn()
+        
