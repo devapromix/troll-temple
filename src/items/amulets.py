0e9fba9bc8ba10f54c86e6c14aca2babe0993ec7
devapromix <maxwof@ukr.net>
2023-12-01 16:34:50 +0200

    improved amulets.

--- src/items/amulets.py
+++ src/items/amulets.py
@@ -22,15 +22,19 @@ class RavenAmulet(Amulet):
         if v == 1:
             if self.suffix("wind"):
                 self.modifier += Mod('speed', 1)
+                self.glyph = '\'', T.white
         elif v == 2:
             if self.suffix("sun"):
                 self.modifier += Mod('radius', 1)
+                self.glyph = '\'', T.light_yellow
         elif v == 3:
             if self.suffix("wolf"):
                 self.modifier += AddMaxLife(rand(3, 9))
+                self.glyph = '\'', T.light_red
         else:
             if self.suffix("spirit"):
                 self.modifier += AddMaxMana(rand(5, 11))
+                self.glyph = '\'', T.light_blue
 
 class WispAmulet(Amulet):
     name = 'amulet'
@@ -43,12 +47,15 @@ class WispAmulet(Amulet):
         if v == 1:
             if self.suffix("reflections"):
                 self.modifier += Mod('reflect_damage_bonus', 25)
+                self.glyph = '\'', T.light_pink
         elif v == 2:
             if self.suffix("tiger"):
                 self.modifier += AddMaxLife(rand(10, 24))
+                self.glyph = '\'', T.light_red
         else:
             if self.suffix("star"):
                 self.modifier += AddMaxMana(rand(12, 29))
+                self.glyph = '\'', T.light_blue
 
 class WardAmulet(Amulet):
     name = 'amulet'
@@ -61,12 +68,15 @@ class WardAmulet(Amulet):
         if v == 1:
             if self.suffix("mirros"):
                 self.modifier += Mod('reflect_damage_bonus', 50)
+                self.glyph = '\'', T.light_pink
         elif v == 2:
             if self.suffix("whale"):
                 self.modifier += AddMaxLife(rand(25, 45))
+                self.glyph = '\'', T.light_red
         else:
             if self.suffix("rainbow"):
                 self.modifier += AddMaxMana(rand(30, 50))
+                self.glyph = '\'', T.light_blue
 
 class RubyAmulet(Amulet):
     ABSTRACT = True
